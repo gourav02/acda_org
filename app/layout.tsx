3068e5d092e5663804e73f4921178284551e930e
gourav mukherjee <gourav.mukherjee@7-11.com>
2025-10-20 01:30:20 +0530

    format fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "ACDA",
-  description: "Together, we envision a healthier tomorrow — where awareness, prevention, and early intervention shape the future of diabetes care.",
+  description:
+    "Together, we envision a healthier tomorrow — where awareness, prevention, and early intervention shape the future of diabetes care.",
 };
 
 export default function RootLayout({
@@ -38,7 +39,7 @@ export default function RootLayout({
               priority="info"
             />
             <Header />
-             <link rel="icon" href="/logos/logo.jpg" />
+            <link rel="icon" href="/logos/logo.jpg" />
             <main className="flex-1">{children}</main>
             <Footer />
           </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Menu, X, Activity, ChevronDown, LogOut, LayoutDashboard } from "lucide-react";
+import { Menu, X, ChevronDown, LogOut, LayoutDashboard } from "lucide-react";
 import { useState, useEffect } from "react";
 import { useSession, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
@@ -62,10 +62,10 @@ export default function Header() {
         {/* Logo */}
         <div className="flex lg:flex-1">
           <Link href="/" className="flex items-center space-x-4">
-             <img
-              src={'/logos/logo.jpg'}
+            <img
+              src={"/logos/logo.jpg"}
               className="h-full w-full md:h-[60px] md:w-[64px]"
-              alt={'acda_logo'}
+              alt={"acda_logo"}
             />
             <span className="text-xl font-bold text-white">
               Asansol Coalfield Diabetes Association
